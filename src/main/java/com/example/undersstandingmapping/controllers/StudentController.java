4ff0c2a6fd61ebb578ce0f02df8606ae1783c270
Vinoth <vinothkumar.pannirselvam@henryschein.com>
2024-02-04 19:12:55 +0530

    Student Registration Done with HTML Template

--- src/main/java/com/example/undersstandingmapping/controllers/StudentController.java
+++ src/main/java/com/example/undersstandingmapping/controllers/StudentController.java
@@ -3,8 +3,9 @@ package com.example.undersstandingmapping.controllers;
 import com.example.undersstandingmapping.models.Student;
 import com.example.undersstandingmapping.repositories.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -21,11 +22,37 @@ public class StudentController {
 
     // I'  doing somehting
 
+    @GetMapping("/students")
+    public ModelAndView getHomepage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("homepage");
+        return modelAndView;
+    }
+
     // actual url to which you're serving the response
     @RequestMapping("/getStudents")
     public List<Student> getStudent(){
 
         return studentRepository.findAll();
+    }
+
+    @GetMapping("/create-student")
+    public ModelAndView getStudentForm() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
+
+    @RequestMapping(method = RequestMethod.POST,  value="/create")
+    public ModelAndView createStudent(Long id, String name, String psp) {
+        Student student = new Student();
+        student.setId(id);
+        student.setName(name);
+        student.setPsp(psp);
+        studentRepository.save(student);
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("success");
+        return modelAndView;
     }
 
     // Transactions 1 and 2 completely.
